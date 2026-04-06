c389dbb16adb76aa58a5d6c756823728afe670e2
ianvicky24@gmail.com <ianvicky24@gmail.com>
2026-04-06 19:51:20 +0700

    Fix Theme

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -12,7 +12,6 @@
 
     <!-- Default -->
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/compiled/css/app-dark.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/compiled/css/auth.css') }}">
 </head>
 
